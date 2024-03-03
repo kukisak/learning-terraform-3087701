a882c33c1cf03b61a09379476cbbce70b59551eb
kukralp <kukral.petr@hypotecnibanka.cz>
2024-03-04 00:37:43 +0100

    load balancer

--- main.tf
+++ main.tf
@@ -73,18 +73,13 @@ module "alb" {
       protocol    = "HTTP"
       port        = 80
       target_type = "instance"
+      target_id    = aws_instance.blog.id
     }
   }
 
   tags = {
     Environment = "dev"
   }
-}
-
-resource "aws_lb_target_group_attachment" "test" {
-  target_group_arn = module.alb.target_groups["ex-instance"].arn
-  target_id        = aws_instance.blog.id
-  port             = 80
 }
 
 module "blog_sg" {
